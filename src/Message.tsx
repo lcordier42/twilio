594aab58c5c4517b4574b9e336e1ef78c00874f6
Ludovic Cordier <ludovic.cordier9@gmail.com>
2018-01-05 14:34:41 +0100

    Add prettier format

--- src/Message.tsx
+++ src/Message.tsx
@@ -1,30 +1,30 @@
 import * as React from "react";
 import * as PropTypes from "prop-types";
 import * as classNames from "classnames";
+
 import "./Message.css";
+
 type MessageProps = {
     author?: string;
     body: string;
     me?: boolean;
 };
+
 type MessageState = {};
+
 class Message extends React.Component<MessageProps, MessageState> {
     render() {
         const classes = classNames("Message", {
             log: !this.props.author,
             me: this.props.me,
         });
-        return React.createElement(
-            "div",
-            { className: classes },
-            this.props.author &&
-                React.createElement(
-                    "span",
-                    { className: "author" },
-                    this.props.author,
-                    ":",
-                ),
-            this.props.body,
+        return (
+            <div className={classes}>
+                {this.props.author && (
+                    <span className="author">{this.props.author}:</span>
+                )}
+                {this.props.body}
+            </div>
         );
     }
 }
