2fe004e0a1876e62930279c0a708a6158f741196
Ludovic Cordier <ludovic.cordier9@gmail.com>
2018-01-08 13:14:30 +0100

    Remove useless files

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,5 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 
 import "./index.css";
-// @ts-ignore
 import App from "./App";
-import registerServiceWorker from "./registerServiceWorker";
-// @ts-ignore
 ReactDOM.render(<App />, document.getElementById("root"));
-registerServiceWorker();
--- twillio.d.ts
+++
@@ -1,5 +0,0 @@
-declare module "twilio-chat";
-declare module "twilsock";
-declare module "twilio-notifications";
-declare module "twilio-transport";
-declare module "twilio-sync";
